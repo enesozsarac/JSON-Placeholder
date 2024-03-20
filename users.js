d69bfab2698df5d85857081027327e952cffbb14
enesozsarac <enesozsarac@gmail.com>
2024-03-20 22:06:18 +0300

    .

--- users.js
+++ users.js
@@ -17,26 +17,27 @@ function createCards(user) {
         <h5>ID:${user.id}</h5>
        </div>
         <div class="buttonContainer">
-          <button onclick = "addressBtn(${user.id})"><i class="fa-solid fa-location-dot"></i></button>
-          <button onclick = "companyBtn(${user.id})"><i class="fa-regular fa-building"></i></i></button>
-          <button onclick = "contactBtn(${user.id})"><i class="fa-regular fa-envelope"></i></i></button>
+          <button onclick = "showBtn(${user.id}, 'address')"><i class="fa-solid fa-location-dot"></i></button>
+          <button onclick = "showBtn(${user.id}, 'company')"><i class="fa-regular fa-building"></i></i></button>
+          <button onclick = "showBtn(${user.id}, 'contact')"><i class="fa-regular fa-envelope"></i></i></button>
+          <a href="post.html?userId=${user.id}"><button><i class="fa-solid fa-comment"></i></button></a>
         </div>
         <div class="content row">
           <div class="address display">
-            <button class="closeBtn" onclick = "closeAddressBtn(${user.id})"><i class="fa-solid fa-x"></i></button>
+            <button class="closeBtn" onclick = "closeBtn(${user.id}, 'address')"><i class="fa-solid fa-x"></i></button>
             Street:${user.address.street}<br>
             Suite:${user.address.suite}<br>
             City:${user.address.city}<br>
             Zipcode:${user.address.zipcode}<br>
           </div>
           <div class="company display">
-            <button class="closeBtn" onclick = "closeCompanyBtn(${user.id})"><i class="fa-solid fa-x"></i></button>
+            <button class="closeBtn" onclick = "closeBtn(${user.id},'company')"><i class="fa-solid fa-x"></i></button>
             Name: ${user.company.name}<br>
             CatchPhrase: ${user.company.catchPhrase}<br>
             Bs: ${user.company.bs}<br>
           </div>
           <div class="contact display">
-            <button class="closeBtn" onclick = "closeContactBtn(${user.id})"><i class="fa-solid fa-x"></i></button>
+            <button class="closeBtn" onclick = "closeBtn(${user.id},'contact')"><i class="fa-solid fa-x"></i></button>
             E-mail: ${user.email}<br>
             Phone: ${user.phone}<br>
             Website: ${user.website}<br>
@@ -47,32 +48,30 @@ function createCards(user) {
 
 getUser();
 
-function addressBtn(id) {
-  const address = document.querySelectorAll(".address");
-  address[id - 1].classList.remove("display");
+function showBtn(id, btn) {
+  if (btn === "address") {
+    const address = document.querySelectorAll(".address");
+    address[id - 1].classList.remove("display");
+  } else if (btn === "company") {
+    const btn = document.querySelectorAll(".company");
+    btn[id - 1].classList.remove("display");
+  } else if (btn === "contact") {
+    const btn = document.querySelectorAll(".contact");
+    btn[id - 1].classList.remove("display");
+  }
 }
 
-function companyBtn(id) {
-  const company = document.querySelectorAll(".company");
-  company[id - 1].classList.remove("display");
+function closeBtn(id, btn) {
+  if (btn === "address") {
+    const btn = document.querySelectorAll(".address");
+    btn[id - 1].classList.add("display");
+  } else if (btn === "company") {
+    const btn = document.querySelectorAll(".company");
+    btn[id - 1].classList.add("display");
+  } else if (btn === "contact") {
+    const btn = document.querySelectorAll(".contact");
+    btn[id - 1].classList.add("display");
+  }
 }
 
-function contactBtn(id) {
-  const contact = document.querySelectorAll(".contact");
-  contact[id - 1].classList.remove("display");
-}
 
-function closeAddressBtn(id) {
-  const address = document.querySelectorAll(".address");
-  address[id - 1].classList.add("display");
-}
-
-function closeCompanyBtn(id) {
-  const company = document.querySelectorAll(".company");
-  company[id - 1].classList.add("display");
-}
-
-function closeContactBtn(id) {
-  const contact = document.querySelectorAll(".contact");
-  contact[id - 1].classList.add("display");
-}
